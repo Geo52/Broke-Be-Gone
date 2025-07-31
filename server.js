5ef52e84af1bfd8adfc126a9683cad5fc66ea24c
Jorge Rios <jorgerios47@gmail.com>
2025-07-31 13:48:13 -0500

    added postgres locally

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-const express = require('express')
+// express
+import express, { response } from 'express'
 const app = express()
-const port = 3000
+// dotenv
+import 'dotenv/config'
+// postgres
+import { pool } from "./auth.js"
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+app.get('/', async(req, res) => {
+    res.send('Hello World!')
 })
 
+
+const port = 3000
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
