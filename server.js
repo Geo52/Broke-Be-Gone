03a444cd68a5c2afaef8879512863190bb6be929
Jorge Rios <jorgerios47@gmail.com>
2025-08-03 08:42:50 -0500

    added port variable for prod

--- server.js
+++ server.js
@@ -212,7 +212,7 @@ app.post('/logout', async (req, res) => {
   res.redirect('/log-in')
 })
 
-const port = 3000
+const port = process.env.PORT || 3000
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
